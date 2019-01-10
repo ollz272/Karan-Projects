71a8278eb984fe71fc7b28e120447e187a5c7bff
ollz272 <46482091+ollz272@users.noreply.github.com>
2019-01-10 14:19:17 +0000

    Inc loop
    This update includes a version of the program that does the same calcualtion, but using loops rather than recursion. Recursion is slow, this is quick.

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
+#include <math.h>
+#include <iomanip>
+
 /*  fibonacci
 */
-double fibbonaci(int n)
+
+
+int fibbonacirecursion(int n)
 {
 	if(n == 0)
 		return 0;
 	if(n == 1)
-	return 1;
-  return fibbonaci(n-1) + fibbonaci (n -2);
+		return 1;
+	return fibbonacirecursion(n-1) + fibbonacirecursion(n -2);
+}
+
+int fibbonaciloop(int n)
+{
+    if(n == 0)
+    {
+		return 0;
+    }
+	if(n == 1)
+	{
+	    return 1;
+	}
+    int storeone = 0;
+    int storetwo = 1;
+    int fibbonaci;
+    for( int i = 1 ; i <= n ; i++)
+    {
+        fibbonaci  = storeone + storetwo;
+        storeone = storetwo;
+        storetwo = fibbonaci;
+    }
+    return fibbonaci;
 }
 
 
@@ -18,7 +45,7 @@ int main()
 	std::cin >> n;
 	for(int i = 1; i <= n; i++)
 	{
-	    std::cout << fibbonaci(i-1) << "," ;
+	    std::cout << fibbonaciloop(i-1) << "," ;
 	}
 	return 0;
 }
